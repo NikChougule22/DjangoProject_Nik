2cdb871b16570d93302526ee82f97c62bf9df7d9
Nikhil <chougulenikhil88@gmail.com>
2022-07-04 00:47:20 -0400

    Worked on creating migration to postgresql

--- travello/models.py
+++ travello/models.py
@@ -1,11 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
 
-class Destination:
-    id : int
-    name : str
-    img : str
-    desc : str
-    price : int
-    offer : bool 
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+    desc = models.TextField()
+    
+    
